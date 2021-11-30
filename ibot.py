9ead5c7b2a50bbdae537b771cf82aa54ba6e97f7
corteisjr <corteisjunior@gmail.com>
2021-11-30 15:05:20 +0200

    Função para criar directorio para adicionar drive

--- ibot.py
+++ ibot.py
@@ -6,3 +6,6 @@ except GetBrowserInfoException:
     print('Não foi possivel idenficar o navegador.')
 else:
     print('Navegador: {0}\nVersão: {1}'.format(binfo['name'], binfo['version']))
+   
+   
+createDir()    
