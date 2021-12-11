5eb9f532c5de85f67e967d0e4291796a590c04f0
ErickCamillo <erick65738@gmail.com>
2021-12-10 22:41:26 -0300

    Adicionada função para analisar pagina de download do webdriver e função para configurar automaticamente o webdriver

--- Modules/browser.py
+++ Modules/browser.py
@@ -1,7 +1,7 @@
 from sys import stderr
 from requests import Session
 from bs4 import BeautifulSoup
-from utils import GetBrowserInfo, GetBrowserInfoException, DEFAULT_PATH, WEBDRIVER_PATH
+from Modules.utils import GetBrowserInfo, GetBrowserInfoException, WEBDRIVER_PATH
 from re import search
 
 BROWSERS_LIST = \
@@ -17,8 +17,6 @@ def __VerifyCurrentBrowser():
     try:
         currentbrowser = GetBrowserInfo()
     except GetBrowserInfoException:
-        print('[INFO] Não foi possivel configurar automaticamente o webdriver', file=stderr)
-        print('[INFO] Informe manualmente o webdriver no arquivo: {}webdriverconfig.json'.format(DEFAULT_PATH), file=stderr)
         return GetBrowserInfoException
     else:
         for browser in BROWSERS_LIST:
@@ -27,16 +25,8 @@ def __VerifyCurrentBrowser():
                 return currentbrowser       
         return None
 
-def __DownloadWebDriver(browser_info):
-
-    request = Session()
-    response = request.get(browser_info['link'])
-    if response.status_code != 200:
-        print('[ERRO] Não foi possivel acessar a pagina de download do webdriver: {0}:{1}'
-        .format(browser_info['name'] , browser_info['link']), file=stderr)
-        return response.status_code
-
-    page = BeautifulSoup(response.text , 'html.parser')
+# Retorna o link de download e nome do webdriver
+def __PageParser(browser_info , page):
 
     if browser_info['name'] == 'firefox':
         box = page.find('div', class_='Box-body')
@@ -49,11 +39,57 @@ def __DownloadWebDriver(browser_info):
 
         for li in page.find_all('li', class_='Box-row'):
             if li.a.span.get_text() == webdriver_title:
-                link = search(r'(\w+.//\w+.\w+)',browser_info['link']).group() + li.a['href']
-                driver = request.get(link)
-                if driver.status_code != 200:
-                    print('[ERRO] Não foi possivel fazer o download do webdriver: {}'.format(webdriver_title), file=stderr)
-                    return driver.status_code
-                with open(WEBDRIVER_PATH + webdriver_title , 'wb') as file:
-                    file.write(driver.content)
-                break
+                url_download = search(r'(\w+.//\w+.\w+)',browser_info['link']).group() + li.a['href']
+                return (webdriver_title , url_download)
+    
+    elif browser_info['name'] == 'chrome':
+        if browser_info['os'] == 'win32':
+            webdriver_title = 'chromedriver_win32.zip'
+        elif browser_info['os'] == 'linux' or browser_info['os'] == 'linux2':
+            webdriver_title = 'chromedriver_linux64.zip'
+
+        spans = page.find_all('span', class_='aw5Odc')
+        for span in spans:
+            webdriver_version = span.a.text.split(' ')[1]
+            webdriver_version_firstnumber = search(r'([0-9])+', webdriver_version).group(0)
+            browser_version_firstnumber = search(r'([0-9])+', browser_info['version']).group(0)
+            if browser_version_firstnumber == webdriver_version_firstnumber:
+                url_download = span.a['href'] + webdriver_title
+                url_download = url_download.replace('index.html?path=', '')
+                return (webdriver_title , url_download)
+                
+def __DownloadWebDriver(browser_info):
+
+    request = Session()
+    response = request.get(browser_info['link'])
+    if response.status_code != 200:
+        print('[ERRO] Não foi possivel acessar a pagina de download do webdriver: {0}:{1}'
+        .format(browser_info['name'] , browser_info['link']), file=stderr)
+        return response.status_code
+
+    page = BeautifulSoup(response.text , 'html.parser')
+
+    webdriver_name , url_download = __PageParser(browser_info , page)
+    driver = request.get(url_download)
+    if driver.status_code != 200:
+        print('[ERRO] Não foi possivel fazer o download do webdriver: {}'.format(webdriver_name), file=stderr)
+        return driver.status_code
+    with open(WEBDRIVER_PATH + webdriver_name , 'wb') as file:
+        file.write(driver.content)
+
+    return 200
+
+def SetWebdriverConfig():
+
+    browserinfo = __VerifyCurrentBrowser()
+    if browserinfo == GetBrowserInfoException:
+        print('[ERRO] Não foi possivel identificar o navegador padrão', file=stderr)
+        return False
+    elif browserinfo == None:
+        print('[INFO] A versão atual não tem suporte para o navegador padrão')
+        return False
+    else:
+        status = __DownloadWebDriver(browserinfo)
+        if status != 200:
+            return False
+    
