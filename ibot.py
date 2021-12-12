af8378691c51a15990989101111f5cb943796eac
ErickCamillo <erick65738@gmail.com>
2021-12-12 02:40:32 -0300

    Adicionado um metodo melhor para verificação da existencia de arquivos necessarios

--- ibot.py
+++ ibot.py
@@ -1,13 +1,19 @@
-from Modules.browser import SetWebdriverConfig
-from Modules.utils import CreateDefaultPath, DEFAULT_PATH
+from Modules.browser import SetWebdriverConfig, GetWebdriverPath
+from Modules.utils import CreateDefaultPath, CONFIG_FILE_NAME
 
-print('[INFO] Verificando diretorios e arquivos necessarios...')
-status = CreateDefaultPath()
-if status == True:
-    print('[INFO] Identificando e configurando navegador padrão...')
-    browsersetting = SetWebdriverConfig()
-    if(browsersetting == False):
-        print('[INFO] Informe manualmente o webdriver no arquivo: {}config.json'.format(DEFAULT_PATH))
-        exit(0)
-else:
-    print('[INFO] Webdriver ja esta configurado')
+print('[INFO] Obtendo caminho até o webdriver...')
+webdriverpath = GetWebdriverPath()
+if webdriverpath == FileNotFoundError:
+    print('[INFO] Não foi possivel encontrar o arquivo de configuração: {}'.format(CONFIG_FILE_NAME))
+    print('[INFO] Criando diretorios e arquivos padrão...')
+    status = CreateDefaultPath()
+    if status:
+        print('[INFO] Diretorios e arquivos criado com sucesso')
+        print('[INFO] Configurando webdriver...')
+        status = SetWebdriverConfig()
+        if status == False:
+            print('[INFO] Informe manualmente o caminho até o webdriver no arquivo: {}'.format(CONFIG_FILE_NAME))
+            exit(1)
+        print('[INFO] Webdriver configurado com sucesso')
+elif webdriverpath == 'null':
+    print('[INFO] Nenhum caminho informado para o webdriver')
