f1a6e32fe244e3702eeb5f72b5a339c65bf26a54
ErickCamillo <erick65738@gmail.com>
2021-12-29 19:54:57 -0300

    Refatoração de codigo

--- Modules/browser.py
+++ Modules/browser.py
@@ -5,101 +5,151 @@ from re import search
 from json import load, dump
 from os.path import basename
 from selenium.webdriver import Firefox , Chrome, Edge
-from Modules.utils import  WebDriverUtils ,GetBrowserInfoException
+from Modules.utils import WebDriverUtils ,GetBrowserInfoException
 
-BROWSERS_LIST = \
-[
-    {'browser_name':'firefox', 'download_link':'https://github.com/mozilla/geckodriver/releases'},
-    {'browser_name':'chrome', 'download_link':'https://chromedriver.chromium.org/downloads'},
-    {'browser_name':'msedge', 'download_link':'https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/'}
-]
+class WebDriverBase:
+    
+    def __init__(self, browserinfo=None):
 
-class WebDriverConfig(WebDriverUtils):
-
-    def __init__(self):
-        WebDriverUtils.__init__(self)
-
-    # Verifica se o navegador padrão esta na lista de navegadores que são suportados.
-    def __VerifyCurrentBrowser(self):
-
-        try:
-            currentbrowser = self.GetBrowserInfo()
-        except GetBrowserInfoException:
-            return GetBrowserInfoException
-        else:
-            for browser in BROWSERS_LIST:
-                if browser['browser_name'] == currentbrowser['name']:
-                    currentbrowser['link'] = browser['download_link']
-                    return currentbrowser       
-            return None
+        if browserinfo != None:
+            self.name = browserinfo['name']
+            self.url_download = browserinfo['url_download']
+            self.version = browserinfo['version']
+            self.os = browserinfo['os']
 
     # Retorna o link de download e nome do webdriver
-    def __PageParser(self, browser_info , page):
+    def __PageParser(self, page):
 
-        if browser_info['name'] == 'firefox':
+        # <Firefox>
+        if self.name == 'firefox':
             box = page.find('div', class_='Box-body')
             webdriver_version = box.find('a', class_='Link--primary').text
 
-            if browser_info['os'] == 'win32':
+            if self.os == 'win32':
                 webdriver_title = 'geckodriver-v{}-win64.zip'.format(webdriver_version)
-            elif browser_info['os'] == 'linux' or 'linux2':
+            elif self.os == 'linux' or self.os == 'linux2':
                 webdriver_title = 'geckodriver-v{}-linux64.tar.gz'.format(webdriver_version)
 
             for li in page.find_all('li', class_='Box-row'):
                 if li.a.span.get_text() == webdriver_title:
-                    url_download = search(r'(\w+.//\w+.\w+)',browser_info['link']).group() + li.a['href']
+                    url_download = search(r'(\w+.//\w+.\w+)', self.url_download).group() + li.a['href']
                     return (webdriver_title , url_download)
+        # </Firefox>
         
-        elif browser_info['name'] == 'chrome':
-            if browser_info['os'] == 'win32':
+        # <Chrome>
+        elif self.name == 'chrome':
+            if self.os == 'win32':
                 webdriver_title = 'chromedriver_win32.zip'
-            elif browser_info['os'] == 'linux' or browser_info['os'] == 'linux2':
+            elif self.os == 'linux' or self.os == 'linux2':
                 webdriver_title = 'chromedriver_linux64.zip'
 
             spans = page.find_all('span', class_='aw5Odc')
             for span in spans:
                 webdriver_version = span.a.text.split(' ')[1]
                 webdriver_version_firstnumber = search(r'([0-9])+', webdriver_version).group(0)
-                browser_version_firstnumber = search(r'([0-9])+', browser_info['version']).group(0)
+                browser_version_firstnumber = search(r'([0-9])+', self.version).group(0)
                 if browser_version_firstnumber == webdriver_version_firstnumber:
                     url_download = span.a['href'] + webdriver_title
                     url_download = url_download.replace('index.html?path=', '')
                     return (webdriver_title , url_download)
+        # </Chrome>
         
+        # <Edge>
         # Implementado o uso do webdriver edge apenas no windows
-        elif browser_info['name'] == 'msedge':
+        elif self.name == 'msedge':
             elements_p = page.find_all('p', class_='driver-download__meta')
             for p in elements_p:
                 webdriver_version = p.text
                 webdriver_version_firstnumber = search(r'([0-9])+', webdriver_version).group(0)
-                browser_version_firstnumber = search(r'([0-9])+', browser_info['version']).group(0)
+                browser_version_firstnumber = search(r'([0-9])+', self.version).group(0)
                 if browser_version_firstnumber == webdriver_version_firstnumber:
                     for a in p.find_all('a'):
                         if a.text == 'x64':
                             url_download = a['href']
                             webdriver_title = basename(url_download)
                             return (webdriver_title , url_download)
-                    
-    def __DownloadWebDriver(self, browser_info):
+        # </Edge>
+
+    def DownloadWebDriver(self, path):
 
         request = Session()
-        response = request.get(browser_info['link'])
+        response = request.get(self.url_download)
         if response.status_code != 200:
             print('[ERRO] Não foi possivel acessar a pagina de download do webdriver: {0}:{1}'
-            .format(browser_info['name'] , browser_info['link']), file=stderr)
+            .format(self.browsername , self.url_download), file=stderr)
             return (response.status_code, None)
 
         page = BeautifulSoup(response.text , 'html.parser')
 
-        webdriver_name , url_download = self.__PageParser(browser_info , page)
+        webdriver_name , url_download = self.__PageParser(page)
         driver = request.get(url_download)
         if driver.status_code != 200:
             print('[ERRO] Não foi possivel fazer o download do webdriver: {}'.format(webdriver_name), file=stderr)
             return (driver.status_code,None)
-        with open(self.WEBDRIVER_PATH + webdriver_name , 'wb') as file:
+        with open(path + webdriver_name , 'wb') as file:
             file.write(driver.content)
 
         return (200, webdriver_name)
+
+class FirefoxWebDriver(WebDriverBase):
+
+    def __init__(self, binfo=None):
+
+        WebDriverBase.__init__(self, browserinfo=binfo)
+
+    def WebDriver(self, path):
+
+        self.driver = Firefox(executable_path=path)
+        return self.driver
+        
+class ChromeWebDriver(WebDriverBase):
+
+    def __init__(self, binfo=None):
+
+        WebDriverBase.__init__(self, browserinfo=binfo)
+
+    def WebDriver(self, path):
+
+        self.driver = Chrome(executable_path=path)
+        return self.driver
+
+class EdgeWebDriver(WebDriverBase):
+
+    def __init__(self, binfo=None):
+
+        WebDriverBase.__init__(self, browserinfo=binfo)
+
+    def WebDriver(self, path):
+
+        self.driver = Edge(executable_path=path)
+        return self.driver
+
+class WebDriverConfig(WebDriverUtils):
+
+    def __init__(self):
+
+        WebDriverUtils.__init__(self)
+        self.BROWSERS_LIST = \
+        [
+            {'browser_name':'firefox', 'url':'https://github.com/mozilla/geckodriver/releases', 'instance': FirefoxWebDriver},
+            {'browser_name':'chrome', 'url':'https://chromedriver.chromium.org/downloads', 'instance': ChromeWebDriver},
+            {'browser_name':'msedge', 'url':'https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/', 'instance': EdgeWebDriver}
+        ]
+
+    # Obtem informarções do navegador atual e retorna uma instancia do webdriver
+    def __GetCurrentBrowser(self):
+
+        try:
+            currentbrowser = self.GetBrowserInfo()
+        except GetBrowserInfoException:
+            raise GetBrowserInfoException
+        else:
+            for item in self.BROWSERS_LIST:
+                if item['browser_name'] == currentbrowser['name']:
+                    currentbrowser['url_download'] = item['url']
+                    instance = item['instance']
+                    return instance(binfo=currentbrowser)      
+            return None
 
     def __SetWebdriverInfo(self, path , browser_name):
 
@@ -121,37 +171,30 @@ class WebDriverConfig(WebDriverUtils):
 
     def SetWebdriverConfig(self):
 
-        browserinfo = self.__VerifyCurrentBrowser()
-        if browserinfo == GetBrowserInfoException:
+        try:
+            browser = self.__GetCurrentBrowser()
+
+            if browser == None:
+                print('[INFO] A versão atual não tem suporte para o navegador padrão')
+                return False
+
+            response_code, webdriver_name = browser.DownloadWebDriver(self.WEBDRIVER_PATH)
+            if response_code == 200:
+                full_path = self.ExtractFile(self.WEBDRIVER_PATH + webdriver_name)
+                self.__SetWebdriverInfo(full_path, browser.name)
+                return (browser, full_path)
+
+            return False
+
+        except GetBrowserInfoException:
             print('[ERRO] Não foi possivel identificar o navegador padrão', file=stderr)
             return False
-        elif browserinfo == None:
-            print('[INFO] A versão atual não tem suporte para fazer a configuração automatica de acordo com o navegador padrão')
-            return False
-        else:
-            response_code, webdriver_name = self.__DownloadWebDriver(browserinfo)
-            if response_code != 200:
-                return False
-            else:
-                full_path = self.ExtractFile(self.WEBDRIVER_PATH + webdriver_name)
-                self.__SetWebdriverInfo(full_path, browserinfo['name'])
-                return (True, {'webdriver':full_path , 'browser':browserinfo['name']})
 
-    # Cria uma instancia do webdriver de acordo com o webdriver configurado
-    # No arquivo DEFAULT_PATH/config.json
-    def WebDriver(self, wdinfo):
-       
-        webdrivers = [{'name':'firefox','instance':Firefox},
-                     {'name':'chrome', 'instance':Chrome},
-                     {'name':'msedge', 'instance':Edge}]
-
-        for webdriver in webdrivers:
-            if webdriver['name'] == wdinfo['browser'].lower():
-                driver = webdriver['instance']
-                return driver(executable_path=wdinfo['webdriver'])
+    def Start(self, browser_name):
         
-        return None
+        for browser in self.BROWSERS_LIST:
+            if browser['browser_name'] == browser_name:
+                instance = browser['instance']
+                return instance()
 
-# Usando a maior quantidade de tecnicas para não ser detectado como bot
-class UndectableMode:
-    pass
+        return None
--- ibot.py
+++ ibot.py
@@ -1,29 +1,46 @@
 from Modules.browser import WebDriverConfig
+from Modules.instagram import Instagram
 
 CHECK_MARK = u'\u2713'
 
-print('[...] Obtendo configurações do webdriver')
-try:
-    webdriverconfig = WebDriverConfig()
-    webdriverinfo = webdriverconfig.GetWebdriverConfig()
-except FileNotFoundError:
-    print('[INFO] Não foi possivel encontrar o arquivo de configuração: {0}{1}'.format(webdriverconfig.DEFAULT_PATH,webdriverconfig.CONFIG_FILE_NAME))
-    print('[...] Criando diretorios e arquivos padrão...')
-    status = webdriverconfig.CreateDefaultPath()
-    if status:
-        print('[ {} ] Diretorios e arquivos criados com sucesso'.format(CHECK_MARK))
-        print('[...] Configurando webdriver')
-        status = webdriverconfig.SetWebdriverConfig()
-        if status == False:
+def StartBrowser():
+
+    try:
+        webdriverconfig = WebDriverConfig()
+        webdriverinfo = webdriverconfig.GetWebdriverInfo()
+    except FileNotFoundError:
+        print('[INFO] Não foi possivel encontrar o arquivo de configuração: {0}{1}'.format(webdriverconfig.DEFAULT_PATH,webdriverconfig.CONFIG_FILE_NAME))
+        print('[...] Criando diretorios e arquivos padrão')
+        status = webdriverconfig.CreateDefaultPath()
+        if status:
+            print('[ {} ] Diretorios e arquivos criados com sucesso'.format(CHECK_MARK))
+            print('[...] Configurando webdriver')
+            status = webdriverconfig.SetWebdriverConfig()
+            if status == False:
+                print('[INFO] Informe manualmente o caminho até o webdriver no arquivo: {0}{1}'.format(webdriverconfig.DEFAULT_PATH,webdriverconfig.CONFIG_FILE_NAME))
+                exit(1)
+            
+            browser_instance = status[0]
+            browser_instance_path = status[1]
+            print('[ {0} ] Webdriver configurado com sucesso: {1} | Navegador: {2}'.format(CHECK_MARK,browser_instance_path , browser_instance.name))
+            return (browser_instance, browser_instance_path)
+    else: 
+        if webdriverinfo['webdriver'] == 'null':
+            print('[INFO] Nenhum webdriver configurado')
             print('[INFO] Informe manualmente o caminho até o webdriver no arquivo: {0}{1}'.format(webdriverconfig.DEFAULT_PATH,webdriverconfig.CONFIG_FILE_NAME))
             exit(1)
-        elif status[0]:
-            webdriverinfo = status[1]
-            print('[ {0} ] Webdriver configurado com sucesso: {1} | Navegador: {2}'.format(CHECK_MARK,webdriverinfo['webdriver'] , webdriverinfo['browser']))
-else: 
-    if webdriverinfo['webdriver'] == 'null':
-        print('[INFO] Nenhum webdriver configurado')
-        print('[INFO] Informe manualmente o caminho até o webdriver no arquivo: {0}{1}'.format(webdriverconfig.DEFAULT_PATH,webdriverconfig.CONFIG_FILE_NAME))
-        exit(1)
-    else:
-        print('[ {0} ] Webdriver encontrado: {1} | Navegador: {2}'.format(CHECK_MARK,webdriverinfo['webdriver'], webdriverinfo['browser']))
+        else:
+            browser_instance = webdriverconfig.Start(webdriverinfo['browser'])
+            if browser_instance == None:
+                print('[INFO] A versão atual não tem suporte para o navegador: {}'.format(webdriverinfo['browser']))
+                exit(1)
+                
+            print('[ {0} ] Webdriver: {1} | Navegador: {2}'.format(CHECK_MARK,webdriverinfo['webdriver'], webdriverinfo['browser']))
+            return (browser_instance, webdriverinfo['webdriver'])
+
+if __name__ == '__main__':
+    
+    print('[...] Iniciando webdriver')
+    browser, browser_path = StartBrowser()
+    driver = browser.WebDriver(browser_path)
+    driver.get('https://www.google.com')
